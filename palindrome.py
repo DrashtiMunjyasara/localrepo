594fff0bb3d3edb5ad9e0d715555bceb0bcf6766
Drashti Munjyasara <drashtirm10@gmail.com>
2025-09-20 19:26:38 +0530

    palindrome program

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def reverse_string(og_string):
+    rev_str=""
+    for char in og_string:
+        rev_str=char+rev_str
+    return rev_str
+
+og=input("Enter the string:")
+og=og.replace(" ","").lower()
+rev_str=reverse_string(og)
+if(og==rev_str):
+    print("palindrome")
+else:
+    print("Not palindrome")
